8991ea72751b193abd8591521c0629068d466d5f
HONG WANG <654514864@qq.com>
2017-01-30 14:21:23 +0000

    User microposting feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+     has_many :microposts, dependent: :destroy
      before_save { self.email = email.downcase }
      validates :name, presence: true, length: { in: 9..30 }
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +8,7 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive: false }
       validates :password, presence: true, length: { minimum: 6 }
       has_secure_password
+       def feed
+        Micropost.where("user_id = ?", id)
+      end  
 end
